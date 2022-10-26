a21235a1e3b8b6f3ab39c70bd179457fbf246e1a
Valerie4792 <vaguilar4792@gmail.com>
2022-10-26 01:50:15 -0400

    added restaurant picker, validation, and playagain option

--- Program.cs
+++ Program.cs
@@ -1,162 +1,82 @@
-﻿using System;
+﻿//Valerie Aguilar
+//October 25, 2022
+bool playAgain = true;
+bool isValid = true;
+bool validNumber;
+string response;
+string userSelect;
 
-namespace RestaurantPicker // Note: actual namespace depends on the project name.
+
+
+string[] Restaurants = { "Nena's", "Xochimilco", "Las Palmas", "Tio Pepe's", "Arroyo's", "Casa Flores", "Las Casuelas", "El Grullense", "Don Luis", "Taco Bell", "Angelina's", "Pietro's", "Olive Garden", "The Waterloo", "De Vega Brother's", "Bella Vista", "String's", "Da'Vinci's", "Eddie's Pizza", "David's Pizza", "Denny's", "iHOP", "Cast Iron Trading", "Chick Fil A", "BJ's Restaurant and BrewHouse", "Moo Moo's", "FED", "The Kitchen at Kitchenaid", "Cast Iron" };
+
+Random RanRest = new Random();
+int choice;
+
+while (playAgain == true)
 {
-    internal class Program
+    isValid = true;
+    Console.WriteLine("Welcome! Please select a restaurnt category");
+    Console.WriteLine("Select 1 for Mexican Food, Select 2 for Italian Food, Select 3 for American Food, or select 4 for all categories combined.");
+
+    do
     {
-        static void Main(string[] args)
+        userSelect = Console.ReadLine();
+        validNumber = int.TryParse(userSelect, out choice);
+        if (validNumber != true || choice >= 5)
         {
-            class AmericanFoodScript
-{
-
-     List<RestaurantClasses> AmerRestaurants = new List<RestaurantClasses>();
-     TextMeshProUGUI AmerDisplayTxt;
-    System.Random AmerRest = new System.Random();
-    int rndAmer;
-    int i = 0;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        AmerDisplayTxt.text = AmerRestaurants[0].RestDisplay; 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-      void AmerFoodbttn()
-    {
-        rndAmer = AmerRest.Next(0,AmerRestaurants.Count);
-        
-        AmerDisplayTxt.text = AmerRestaurants[rndAmer].RestDisplay;
-    } 
-
-}
-
- class ItalianFoodScript
-{
-
-     List<RestaurantClasses>  ItlRestaurants = new List<RestaurantClasses>();
-     TextMeshProUGUI ItlDisplayTxt;
-    System.Random ItlRest = new System.Random();
-    int rndItl;
-    int i = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        ItlDisplayTxt.text = ItlRestaurants[0].RestDisplay;
-    }
-
-      void ItlFoodbttn()
-    {
-        rndItl = ItlRest.Next(0,ItlRestaurants.Count);
-
-        ItlDisplayTxt.text = ItlRestaurants[rndItl].RestDisplay;
-    }
-}
-
- class MexicanFoodScript
-{
-    List<RestaurantClasses>  MexRestaurants = new List<RestaurantClasses>();
-     TextMeshProUGUI MexDisplayTxt;
-    System.Random MexRest = new System.Random();
-    int rndMex;
-    int i = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        MexDisplayTxt.text = MexRestaurants[0].RestDisplay;
-    }
-        void MexFoodbttn()
-    {
-        rndMex = MexRest.Next(0,MexRestaurants.Count);
-        MexDisplayTxt.text = MexRestaurants[rndMex].RestDisplay;
-    }
-}
- class RestaurantPicker
- {
- 
-     List<RestaurantClasses>AllRestaurants = new List<RestaurantClasses>();
-    //added list function tied to my class, RestaurantClasses, list name is All restaurants
-    
-     TextMeshProUGUI RandDisplayTxt;
-    int i = 0;
-    int rndAll;
-    //added sprites for my background images
-     Sprite amerBG;
-     Sprite mexBG;
-     Sprite itlBG;
-     Image BGlayer;
-    System.Random randomRest = new System.Random();
-    //declared Variables above
-   
-    
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //test to display first restaurant element in start function
-        RandDisplayTxt.text = AllRestaurants[0].RestDisplay; 
-        // BGlayer.sprite = mexBG;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-//below function was created to allow SceneChanges, and to connect the function to our Unity Side
-     void SceneChange (string RestScene)
-    {
-    SceneManager.LoadScene(RestScene);
-    }
-
-//function to generate random food from all 3 restaurant categories
-     void AllFoodbttn()
-    {
-        
-
-        rndAll = randomRest.Next(0,AllRestaurants.Count);
-
-        RandDisplayTxt.text = AllRestaurants[rndAll].RestDisplay;
-
-        BGchange(rndAll);
-        //Called function BGchange, and passed in variable rndAll
-        
-       
-    }  
-    //created a function for background change and passed in/declared a variable (rndAll) with data type int
-    void BGchange (int rndAll)
-    {
-         if( rndAll <= 9)
-        {//if rndAll is les than or equal to 9, will display Mexican Food Background image
-            BGlayer.sprite = mexBG;
-        }
-        else if (rndAll>=10 && rndAll<= 19)
-        { //if rndAll is greater than or equal to 10, and less than or equal to 19, will display Italian Food Background image
-            BGlayer.sprite = itlBG;
-        }
-        else
-        {//else if all above are false, will display American food Background
-            BGlayer.sprite = amerBG;
+            Console.Write("Please enter a valid whole number.");
         }
     }
+    while (validNumber != true || choice >= 5);
 
-
-}
-
-
-            Console.WriteLine("Hello World!");
-        }
+    if (choice == 1)
+    {
+        int ranGen = RanRest.Next(0, 10);
+        Console.WriteLine("Your restaurant is: " + Restaurants[ranGen]);
     }
-}
+    else if (choice == 2)
+    {
+        int ranGen2 = RanRest.Next(10, 20);
+        Console.WriteLine("Your restaurant is: " + Restaurants[ranGen2]);
+    }
+    else if (choice == 3)
+    {
+        int ranGen3 = RanRest.Next(20, 30);
+        Console.WriteLine("Your restaurant is: " + Restaurants[ranGen3]);
+    }
+    else if (choice == 4)
+    {
+        int ranGen4 = RanRest.Next(0, 30);
+        Console.WriteLine("Your restaurant is: " + Restaurants[ranGen4]);
+    }
+    while(isValid)
+    {
+        Console.Write("Do you want to select again? (Y/N) ");
+        response = Console.ReadLine().ToUpper();
+        if(response == "Y")
+            {
+                playAgain = true;
+                isValid = false;
+            }
+            else if(response == "N")
+            {
+                playAgain = false;
+                isValid = false;
+            }
+            else
+            {
+                playAgain = false;
+                isValid = true;
+                Console.WriteLine(response + " isn't a valid response. Please Enter an actual valid response please.");
+            }
+        
 
+    }
+
+
+
+}
 
 
 
